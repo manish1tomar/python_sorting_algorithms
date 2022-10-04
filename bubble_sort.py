6b22ee6b8d6689cbb200273aa20ffd950d6cb26e
manish1tomar <manish1.tomar@gmail.com>
2022-10-03 22:49:40 -0500

    Not to sort if list is already sorted

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,13 @@
 def bubble_sort(list):
+    sorted = False
     for i in range(len(list)):
-        for j in range(i+1,len(list),1):
-            if list[i] > list[j]:
-                list[i], list[j] = list[j], list[i]
+        if sorted == False:
+            sorted = True
+            for j in range(i+1,len(list),1):
+                if list[i] > list[j]:
+                    list[i], list[j] = list[j], list[i]
+                    sorted = False
     return list
 
 print(bubble_sort([5,6,3,8,7,5,1,2,3]))
+print(bubble_sort([1,2,3,4,5]))
