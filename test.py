8eb76b2abe5fa2bba18805a4697f2881dd6e27d8
manish1tomar <manish1.tomar@gmail.com>
2022-10-07 17:53:18 -0500

    Initial Version of Heapsort

--- test.py
+++ test.py
@@ -1,3 +1,31 @@
-list = [1,2,3,4,5]
-del list[2]
-print(list)
+class Base(object):
+    def __init__(self):
+        print("Base initiated")
+
+class ChildA(Base):
+    def __init__(self):
+        print("Child A Initiated")
+        Base.__init__(self)
+
+class ChildB(Base):
+    def __init__(self):
+        print("Child B Initiated")
+        super().__init__()
+
+class UserDependency(Base):
+    def __init__(self):
+        print("UserDependency Initiated")
+        super().__init__()
+    
+class UserA(ChildA, UserDependency):
+    def __init__(self):
+        print("User A initiated")
+        super().__init__()
+
+class UserB(ChildB, UserDependency):
+    def __init__(self):
+        print("User B initiated")
+        super().__init__()
+
+UserA()
+UserB()
