b9c7cbb9d536ba39a15f6d600114a84c30b4fadc
manish1tomar <manish1.tomar@gmail.com>
2022-10-23 00:19:27 -0500

    3 methods for two_number_sum

--- two_number_sum.py
+++ two_number_sum.py
@@ -1,17 +1,36 @@
+# Time O(N2), Space O(1)
 def twoNumberSum(array, targetSum):
-    ret_array = []
+    for i in range(len(array) -1):
+        for j in range(i+1,len(array)):
+            firstNum = array[i]
+            secondNum = array[j]
+            if firstNum + secondNum == targetSum:
+                return [firstNum, secondNum]
+    return []
+
+# Time O(N), Space O(N)
+def twoNumberSum_(array, targetSum):
     dict = {}
-    other_num = targetSum
-    element = None
-
     for i in array:
-        dict[i] = 'Found'
+        firstNum = i
+        secondNum = targetSum - firstNum
+        if secondNum in dict.keys():
+            return [firstNum, secondNum]
+        else:
+            dict[i] = True
+    return []
 
-    for i in array:
-        other_num = targetSum - i
-        if other_num != i:
-            if other_num in dict:
-                if i not in ret_array:
-                    ret_array.extend((i, other_num))
-    
-    return ret_array
+# Time O(NlogN), Space O(1)
+def twoNumberSum__(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) -1
+    while left < right:
+        potentialSum = array[left] + array[right]
+        if potentialSum == targetSum:
+            return [array[left], array[right]]
+        elif potentialSum > targetSum:
+            right -= 1
+        elif potentialSum < targetSum:
+            left += 1
+    return []
