c9a357d4a54388f2e59fbfab7cc4085501614f47
manish1tomar <manish1.tomar@gmail.com>
2022-10-25 22:09:54 -0500

    Improved version

--- validate_subsequence.py
+++ validate_subsequence.py
@@ -1,43 +1,13 @@
+#O(N) Time Complexity, O(1) Space Complexity
 def isValidSubsequence(array, sequence):
-    print(array, sequence)
-    dict = {}
-    idx = None
-    for i,j in enumerate(array):
-        if j in dict:
-            l = dict[j]
-            l.extend([i])
-            dict[j] = l
-        else:
-            dict[j] = [i]
-    
-    if len(sequence) > len(array):
-        return False
-    elif sequence == array:
-        return True
-    elif len(array) > 0 and len(sequence) == 0:
-        return False
+    i = 0; j = 0
+    while i < len(sequence):
+        if sequence[i] == array[j]:
+            i += 1
+        j += 1
 
-    for i,j in enumerate(sequence):
-        if j not in dict:
+        if j == len(array) and i < len(sequence):
             return False
-        else:
-            if idx is None:
-                idx = dict[j][0]
-                if len(dict[j]) == 1:
-                    del(dict[j])
-                else:
-                    dict[j].pop(0)
-            
-            elif idx > dict[j][0]:
-                return False
-
-            else:
-                idx = dict[j][0]
-                if len(dict[j]) == 1:
-                    del(dict[j])
-                else:
-                    dict[j].pop(0)  
-                              
     return True
 
 print(isValidSubsequence( [1, 1, 1, 1, 1], [1, 1, 1] ))
